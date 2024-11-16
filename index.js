7450bf7b4038ac051ca039ed41499bef95966342
riyanfazrirahman <riyanfazrirahman@gmail.com>
2024-11-16 12:57:10 +0700

    update code 4.0

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { MyBot } from "./api/api-bot-telegram.js";
+import TelegramBot from "node-telegram-bot-api";
 import { getGempaBMKG } from "./api/api-gempa.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import fetch from "node-fetch";
 import dotenv from "dotenv";
 import path from "path";
 
@@ -10,6 +11,81 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const option = { polling: true };
+
+// Inisialisasi bot Telegram
+const MyBot = new TelegramBot(token, option);
+
+const prefix = "/";
+
+// Pesan pertama bot
+const startBot = new RegExp(`^${prefix}start$`);
+MyBot.onText(startBot, (callback) => {
+  const startMessage = `
+Halo! Saya adalah bot informasi gempa.
+Gunakan /gempa untuk mendapatkan info gempa terkini.
+
+Bot ini menyediakan data kejadian gempa bumi yang terjadi di seluruh wilayah Indonesia, yang terdiri dari tiga jenis data kejadian gempa:
+
+<b>1. Gempabumi M 5.0+</b> – Gempabumi dengan magnitudo 5.0 ke atas.
+<b>2. Gempabumi Dirasakan</b> – Gempabumi yang dirasakan oleh masyarakat.
+<b>3. Gempabumi Berpotensi Tsunami</b> – Gempabumi yang berpotensi menimbulkan tsunami.
+
+Data yang saya gunakan berasal dari Gempabumi Terbaru yang tersedia di file <code>autogempa.json</code> milik BMKG (Badan Meteorologi, Klimatologi, dan Geofisika).
+  `;
+
+  MyBot.sendMessage(callback.from.id, startMessage, { parse_mode: "HTML" });
+});
+
+// Menangani permintaan /gempa untuk mendapatkan informasi gempa
+const gempa = new RegExp(`^${prefix}gempa$`);
+MyBot.onText(gempa, async (callback) => {
+  const BMKG_ENDPOINT = "https://data.bmkg.go.id/DataMKG/TEWS/";
+
+  try {
+    const apiCall = await fetch(BMKG_ENDPOINT + "autogempa.json");
+    const data = await apiCall.json();
+
+    const {
+      Infogempa: {
+        gempa: {
+          Tanggal,
+          Jam,
+          Magnitude,
+          Kedalaman,
+          Wilayah,
+          Potensi,
+          Shakemap,
+        },
+      },
+    } = data;
+
+    const BMKGImage = BMKG_ENDPOINT + Shakemap;
+
+    const resultText = `
+<i>${Tanggal} | ${Jam}</i>
+Besaran Gempa: <b>${Magnitude} SR</b> | Kedalaman: <b>${Kedalaman}</b>.
+Lokasi: ${Wilayah}.
+Potensi: ${Potensi}.
+
+<i>Sumber data: </i>
+<b>BMKG</b> <code>(Badan Meteorologi, Klimatologi, dan Geofisika)</code>
+    `;
+
+    MyBot.sendPhoto(callback.from.id, BMKGImage, {
+      caption: resultText,
+      parse_mode: "HTML",
+    });
+  } catch (error) {
+    console.error("Error fetching earthquake data:", error);
+    MyBot.sendMessage(
+      callback.from.id,
+      "Terjadi kesalahan saat mendapatkan data gempa."
+    );
+  }
+});
 
 // Menggunakan `import.meta.url` untuk mendapatkan __dirname dalam ES Modules
 const __filename = fileURLToPath(import.meta.url);
